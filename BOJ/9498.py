a30211c23412e3adc00ad7d55c492956f12d0d07
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-06-20 22:14:36 +0900

    Update 9498.py

--- BOJ/9498.py
+++ BOJ/9498.py
@@ -1,12 +1,12 @@
 score = int(input())
 
-if (score >= 90) & (score <= 100):
+if score >= 90:
   print('A')
-elif (score >= 80) & (score < 90):
+elif score >= 80:
   print('B')
-elif (score >= 70) & (score < 80):
+elif score >= 70:
   print('C')
-elif (score >= 60) & (score < 70):
+elif score >= 60:
   print('D')
 else:
   print('F')
