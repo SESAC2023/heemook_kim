2680cd350a614a1a54efbfcf4f7e9142cb6ffde4
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-06-20 22:16:24 +0900

    Update 2753.py

--- BOJ/2753.py
+++ BOJ/2753.py
@@ -1,10 +1,6 @@
 year = int(input())
 
-if year % 400 == 0:
-  print(1)
-elif year % 4 == 0 and year % 100 == 0:
-  print(0)
-elif year % 4 == 0:
+if year % 4 == 0 and (y % 100 != 0 or y % 400 == 0):
   print(1)
 else:
   print(0)
