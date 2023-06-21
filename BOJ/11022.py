5a8a4cafa55bd240f2578aaf5066ca051d27d2b4
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-06-21 12:51:00 +0900

    Create 11022.py

---
+++ BOJ/11022.py
@@ -0,0 +1,8 @@
+# BOJ 11022번
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(1, t + 1):
+    a, b = map(int, input().split())
+    print(f'Case #{_}: {a} + {b} = {a + b}')
