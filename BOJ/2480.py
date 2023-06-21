13a050a02f8dbee4f0d9c1cf7c0c51cb9aed3b94
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-06-21 12:26:21 +0900

    Create 2480.py

---
+++ BOJ/2480.py
@@ -0,0 +1,13 @@
+# BOJ 2480번
+a, b, c = map(int, input().split())
+
+if a == b and b == c:
+    print(10000 + a * 1000)
+elif a == b and a != c:
+    print(1000 + a * 100)
+elif a == c and a != b:
+    print(1000 + a * 100)
+elif b == c and a != b:
+    print(1000 + b * 100)
+else:
+    print(max(a, b, c) * 100)
