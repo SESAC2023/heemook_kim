0bbe04d303dd69bc36f867d495648585d628b504
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-06-21 15:13:01 +0900

    Update 10811.py

--- BOJ/10811.py
+++ BOJ/10811.py
@@ -1,10 +1,12 @@
 # BOJ 10811번
-n, m = map(int, input().split())
-basket = [0] + [i for i in range(1, n + 1)]
+n, m = map(int, input().split(' '))
+
+basket = list(range(1, n+1))
 
 for _ in range(m):
-    i, j = map(int, input().split())
-    basket[i : j + 1] = basket[j : i - 1 : -1]
-
-basket.pop(0)
-[print(_, end=' ') for _ in basket]
+    i, j = map(int, input().split(' '))
+    reverse = basket[i-1:j]
+    basket[i-1:j] = reverse[::-1]
+    
+for _ in basket:
+    print(_, end=' ')
