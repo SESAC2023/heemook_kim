2b94e98983776f8bd55d9c038535aaae22c6cd1c
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-07-03 16:31:41 +0900

    Create 11650.py

---
+++ BOJ/11650.py
@@ -0,0 +1,11 @@
+# BOJ 11650번
+N = int(input())
+
+coordinates = []
+for _ in range(N):
+    x, y = map(int, input().split())
+    coordinates.append((x, y))
+    
+coordinates.sort()
+for coordinate in coordinates:
+    print(coordinate[0], coordinate[1])
