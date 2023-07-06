31d055b18781876fe42802569ba66526f25da541
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-07-06 15:38:17 +0900

    여기 질문 있어요!!!!!

--- BOJ/1012.py
+++ BOJ/1012.py
@@ -1,3 +1,5 @@
+# 틀린 답
+"""
 # BOJ 1012번
 import sys
 sys.setrecursionlimit(int(1e6))
@@ -64,4 +66,39 @@ while t:
                 dfs(nx, ny)
     
     print(bfs(0, 0))
+    t -= 1
+    """
+# 정답
+import sys
+sys.setrecursionlimit(int(1e6))
+t = int(input())
+
+while t:
+    n, m, k = map(int, input().split())
+    
+    graph = [[0] * m for _ in range(n)]
+    for _ in range(k):
+        u, v = map(int, input().split())
+        graph[u][v] = 1
+    
+    visited = [[False] * m for _ in range(n)]
+    
+    def dfs(x, y):
+        if x < 0 or x >= n or y < 0 or y >= m:
+            return False
+        if not visited[x][y] and graph[x][y] == 1:
+            visited[x][y] = True
+            dfs(x - 1, y)
+            dfs(x + 1, y)
+            dfs(x, y - 1)
+            dfs(x, y + 1)
+            return True
+    
+    result = 0
+    for i in range(n):
+        for j in range(m):
+            if dfs(i, j) == True:
+                result += 1
+    
+    print(result)    
     t -= 1
