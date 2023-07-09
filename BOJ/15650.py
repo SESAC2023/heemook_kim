ec2c3a3ce1b26772557b109242d7912563a6411c
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-07-09 20:23:27 +0900

    Update 15650.py

--- BOJ/15650.py
+++ BOJ/15650.py
@@ -13,24 +13,25 @@
 --------------------------------------
 # BOJ 15650번
 n, m = map(int, input().split())
-
-visited = [False] * (n + 1)
+arr = [_ for _ in range(1, n + 1)]
+visited = [False] * n
 
 selected = []
 
 def backtracking(depth, now):
     if depth == m:
-        for x in selected:
-            print(x, end=' ')
+        for i in selected:
+            print(i, end=' ')
         print()
-        return    
+        return
     
-    for i in range(now + 1, n + 1): # now 부터 시작하면 0부터 되므로 1부터로 맞춰주려고 now + 1
-        if not visited[i]:
-            visited[i] = True
-            selected.append(i)
-            backtracking(depth + 1, i)
-            visited[i] = False
-            selected.pop()
+    for j in range(now, n):
+        if visited[j]:
+            continue
+        visited[j] = True
+        selected.append(arr[j])
+        backtracking(depth + 1, j)
+        visited[j] = False
+        selected.pop()
 
 backtracking(0, 0)
