fe9fdff537a43c22b701666be57f16851c965331
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-07-09 19:34:37 +0900

    Update 15649.py

--- BOJ/15649.py
+++ BOJ/15649.py
@@ -11,9 +11,11 @@
 #     for j in i:
 #         print(j, end=' ')
 #     print()
-
+-------------------------------------
 # BOJ 15649번
 n, m = map(int, input().split())
+
+visited = [False] * (n + 1)
 
 selected = []
 
@@ -22,11 +24,14 @@ def backtracking(depth):
         for x in selected:
             print(x, end=' ')
         print() # 간단하게 줄 바꾸는 방법
-        return
-
+        return    
+    
     for i in range(1, n + 1):
-        selected.append(i)
-        backtracking(depth + 1)
-        selected.pop() # 재귀를 마치고 다시 진행되기 때문에 append 된 숫자는 결국 전부 pop 된다!!
+        if not visited[i]:
+            visited[i] = True
+            selected.append(i)
+            backtracking(depth + 1)
+            visited[i] = False
+            selected.pop() # 재귀를 마치고 다시 진행되기 때문에 append 된 숫자는 결국 전부 pop 된다!!
 
 backtracking(0)
