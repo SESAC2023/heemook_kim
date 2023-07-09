547689be9ba9475b8dbbc2b252233c3653e8dd1c
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-07-09 19:20:40 +0900

    Update 15649.py

--- BOJ/15649.py
+++ BOJ/15649.py
@@ -1,13 +1,32 @@
 # BOJ 15649번
-from itertools import permutations
+# from itertools import permutations
 
+# n, m = map(int, input().split())
+# arr = [_ for _ in range(1, n + 1)]
+
+# answer = [] 
+# answer.extend(permutations(arr, m))
+
+# for i in answer:
+#     for j in i:
+#         print(j, end=' ')
+#     print()
+
+# BOJ 15649번
 n, m = map(int, input().split())
-arr = [_ for _ in range(1, n + 1)]
 
-answer = [] 
-answer.extend(permutations(arr, m))
+selected = []
 
-for i in answer:
-    for j in i:
-        print(j, end=' ')
-    print()
+def backtracking(depth):
+    if depth == m:
+        for x in selected:
+            print(x, end=' ')
+        print() # 간단하게 줄 바꾸는 방법
+        return
+
+    for i in range(1, n + 1):
+        selected.append(i)
+        backtracking(depth + 1)
+        selected.pop() # 재귀를 마치고 다시 진행되기 때문에 append 된 숫자는 결국 전부 pop 된다!!
+
+backtracking(0)
