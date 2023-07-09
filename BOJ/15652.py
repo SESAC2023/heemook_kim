bff65f319b70ecf725bb0ebce148a20c7f7c3e37
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-07-09 20:21:32 +0900

    Update 15652.py

--- BOJ/15652.py
+++ BOJ/15652.py
@@ -1,14 +1,39 @@
 # BOJ 15652번
-from itertools import combinations_with_replacement
+# from itertools import combinations_with_replacement
 
+# n, m = map(int, input().split())
+
+# arr = [_ for _ in range(1, n + 1)]
+
+# answer = []
+# answer.extend(combinations_with_replacement(arr, m))
+
+# for i in answer:
+#     for j in i:
+#         print(j, end=' ')
+#     print()
+------------------------------------------------------
+# BOJ 15652번
 n, m = map(int, input().split())
-
 arr = [_ for _ in range(1, n + 1)]
+visited = [False] * n
 
-answer = []
-answer.extend(combinations_with_replacement(arr, m))
+selected = []
 
-for i in answer:
-    for j in i:
-        print(j, end=' ')
-    print()
+def backtracking(depth, now): # 조합
+    if depth == m:
+        for i in selected:
+            print(i, end=' ')
+        print()
+        return
+    
+    for j in range(now, n):
+        # if visited[j]: # 방문 처리 삭제 => 중복 허용
+        #     continue
+        # visited[j] = True
+        selected.append(arr[j])
+        backtracking(depth + 1, j)
+        # visited[j] = False
+        selected.pop()
+
+backtracking(0, 0)
