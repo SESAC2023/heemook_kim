a760d0ff51995986f08d6f1be77042c5098522f6
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-07-12 17:53:28 +0900

    Update 11659.py

--- BOJ/11659.py
+++ BOJ/11659.py
@@ -5,14 +5,13 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 
 arr = list(map(int, input().split()))
-sum_arr = [0] * (n + 1)
-result = 0
-for i in range(1, n + 1): # 다이나믹 프로그래밍 처럼 누적합을 미리 계산해 둔다.
-    result += arr[i - 1]
-    sum_arr[i] = result
 
-# print(sum_arr)
+dp = [0] * (n + 1)
+for i in range(1, n + 1):
+    dp[i] = dp[i - 1] + arr[i - 1] # 점화식으로 나타낼 수 있다 => 다이나믹 프로그래밍 (누적합을 미리 계산해 두기)
+
+# print(dp)
 
 for _ in range(m):
     i, j = map(int, input().split())
-    print(sum_arr[j] - sum_arr[i - 1]) # dp 테이블에서 두 값만 가져와서 계산 => 속도 업!
+    print(dp[j] - dp[i - 1]) # dp 테이블에서 두 값만 가져와서 계산 => 속도 업!
