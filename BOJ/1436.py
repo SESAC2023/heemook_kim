a8ce21d98d91b0d506375f5e8605d6d33fe3f6f6
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-07-17 13:33:55 +0900

    Create 1436.py

---
+++ BOJ/1436.py
@@ -0,0 +1,15 @@
+# BOJ 1436번
+# # 정답처리 되지만 너무 느림
+# n = int(input())
+# x = '666'
+# 종말의수 = [i for i in range(666, 10000666) if str(i).count(x) >= 1]
+# print(종말의수[n - 1])
+-----------------------
+n = int(input())
+target = 666
+while n:
+    if '666' in str(target):
+        n -= 1
+    target += 1
+
+print(target)
