5c2f81c0218b1d981454ae4b92552e6d814c2d96
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-08-19 20:20:47 +0900

    Update 1010.py

--- BOJ/1010.py
+++ BOJ/1010.py
@@ -1,5 +1,29 @@
 # BOJ 1010번
 import sys
+input = sys.stdin.readline
+
+t = int(input().rstrip())
+
+while t:    
+    n, m = map(int, input().split())
+    """
+    mCn 직접 구현
+    """
+    N = 1
+    M = 1
+    K = 1
+    for i in range(1, m + 1):
+        M *= i
+    for j in range(1, n + 1):
+        N *= j
+    for k in range(1, m - n + 1):
+        K *= k
+    
+    print(int(M/(N * K)))    
+    t -= 1
+====================================
+# math 라이브러리 이용
+import sys
 from math import comb
 input = sys.stdin.readline
 
@@ -7,11 +31,8 @@ t = int(input().rstrip())
 
 while t:    
     n, m = map(int, input().split())
-    
-    answer = comb(m, n)
-    
-    print(answer)
-    
+    answer = comb(m, n)    
+    print(answer)    
     t -= 1
 ====================================
 # 백트래킹 => 시간 초과
