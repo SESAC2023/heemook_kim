bed66e34f83e03ddc754303b83acf2ace26f79c5
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-08-23 03:04:47 +0900

    Create 2108.py

--- BOJ/2108.py
+++ BOJ/2108.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 n = int(input().rstrip())
 
 arr = []
-cnt = [0] * 8001 # -4000부터 4000까지 정수 카운트
+cnt = [0] * 8001 # -4000부터 4000까지 정수 카운트 (문제 범위)
 for _ in range(n):
     x = int(input().rstrip())
     arr.append(x)
@@ -15,10 +15,10 @@ arr = sorted(arr)
 max_cnt = [i - 4000 for i in range(8001) if cnt[i] == max(cnt)] # 최빈값만 저장
 
 # mean
-print(int(round(sum(arr) / n, 0)))
+print(round(sum(arr) / n))
 # median
 print(arr[n//2])
 # mode
-print(max_cnt[0]) if len(max_cnt) == 1 else print(max_cnt[1])        
+print(max_cnt[0] if len(max_cnt) == 1 else max_cnt[1])
 # range (max - min)
 print(arr[-1] - arr[0])
